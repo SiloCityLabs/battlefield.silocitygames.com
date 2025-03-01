e83deeab98008fba14d3a317cca6cdab6df377b3
Bana0615 <aelbaneh0615@gmail.com>
2025-03-01 11:06:38 +0000

    [ISSUE-3] Created harline card and changleog page

---
+++ components/changelog/Changelog2025.tsx
@@ -0,0 +1,13 @@
+import { Row, Col } from "react-bootstrap";
+
+function Changelog2025() {
+  return (
+    <>
+      <Row>
+        <Col></Col>
+      </Row>
+    </>
+  );
+}
+
+export default Changelog2025;
